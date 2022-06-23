e73873f98ea1f13f590b17ff05d76239673d60b1
Jonathan-Fernandes-EngComp <108033810+Jonathan-Fernandes-EngComp@users.noreply.github.com>
2022-06-22 22:58:40 -0300

    Update Ai_que_dilica.c

--- Ai_que_dilica.c
+++ Ai_que_dilica.c
@@ -6,33 +6,74 @@
 int main(void)
 {
 	setlocale(LC_ALL, "Portuguese");
-	
+	//******************* Inicio Criação das variaveis *************************/
  	typedef struct
 	{
  		char nome[30];
  		char ingrediente[30][30];
  		char passos[30][50];
 	} receita;
-	
 	receita bolo[30], trufa[30], milkshake[30], pastel[30], pizza[30];
+	int opcao, opcao2, i=0;
 	
-	int opcao, i=0;
+	/*Escrever
+	FILE *file;
+	file = fopen("teste.txt", "w");
+	fprintf(file,"é menino?");
+	fclose(file);
 	
+	Ler
+	FILE *file;
+	file = fopen("teste.txt", "w");
+	if(file == NULL){
+		printf("Arquivo nao pode ser aberto");
+		system("pause");
+		return 0;
+	}
+	char frase[100];
+	while(fgets(frase, 100, file) != NULL){
+		printf("%s", frase);
+	}
+	fclose(file);
+	system("pause");
+	return 0;
+	
+	Incluir Dados
+	FILE *file;
+	file = fopen("teste.txt", "a");
+	if(file == NULL){
+		printf("Arquivo nao pode ser aberto");
+		system("pause");
+		return 0;
+	}
+	char frase[] = "Segunda linha";
+	fputs(frase, file);
+	fclose(file);
+	system("pause");
+	return 0;
+	
+	/******************** Fim Criação das variaveis ********************/
+	/******************** Inicio Programa ********************/
 	do{
 		puts("\t\t\t\t\t\t---------------\n\t\t\t\t\t\t|AI QUI DILICA|\n\t\t\t\t\t\t---------------\n");
-		printf("\t\t\t\t\t---    ----------------------\n\t\t\t\t\t|1| -> | Consultar receitas |\n\t\t\t\t\t---    ----------------------\n\t\t\t\t\t---    ---------------------\n\t\t\t\t\t|2| -> | Cadastrar receita |\n\t\t\t\t\t---    ---------------------\n\t\t\t\t\t---    --------------------\n\t\t\t\t\t|3| -> | Sair do programa |\n\t\t\t\t\t---    --------------------\n\t\t\t\t\t-> Opcao: ");
+		printf("\t\t\t\t\t---    ----------------------\n\t\t\t\t\t|1| -> | Cadastrar receita |\n\t\t\t\t\t---    ----------------------\n\t\t\t\t\t---    ---------------------\n\t\t\t\t\t|2| -> | Consultar receita |\n\t\t\t\t\t---    ---------------------\n\t\t\t\t\t---    --------------------\n\t\t\t\t\t|3| -> | Sair do programa |\n\t\t\t\t\t---    --------------------\n\t\t\t\t\t-> Opção: ");
 		scanf("%d", &opcao);
 		
 		switch (opcao)
 		{
 		case 1:
-			printf("Digite o tipo ");
-				
+			printf("\n-> Escolha o tipo de receita que você quer cadastrar:\n\n");
+			printf("---    --------\n|1| -> | Bolo |\n---    ---------\n|2| -> | Trufa |\n---    -------------\n|3| -> | Milkshake |\n---    -------------\n|4| -> | Pastel |\n---    ----------\n|5| -> | Pizza |\n---    ---------\n\n-> Opção: \n\n");
+			scanf("%d", &opcao2);
+			switch (opcao2){
+				case 1:
+					printf("Digite o nome da receita: ");
+					scanf(" %[^\n]s", bolo[0].nome);
+					printf(" %s", bolo[0].nome);
+			}
 			break;
 		case 2:
-			printf("\n-> Escolha qual alimento dentre os seguintes voce deseja cadastrar uma receita nova:\n\n");
-			printf("---    --------\n|1| -> | Bolo |\n---    ---------\n|2| -> | Trufa |\n---    -------------\n|3| -> | Milkshake |\n---    -------------\n|4| -> | Pastel |\n---    ----------\n|5| -> | Pizza |\n---    ---------\n\n-> Opcao: \n\n");
-			
+			printf("Digite o tipo ");
 			break;
 		}
 	} while(opcao!=3);
